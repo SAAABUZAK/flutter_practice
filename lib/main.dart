06ebea70eb817bdb6a77f5a7bff44096b24f80f3
SAAABUZAK <hibabel1219@gmail.com>
2023-08-16 09:03:24 +0900

    🧑‍💻 add progress bar example

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
-      home: const ButtonExample(),
+      home: const ProgressBarExample(),
     );
   }
 }
@@ -29,6 +29,42 @@ class MyHomePage extends StatelessWidget {
             children: [Text('Hello'), Text('World'), Text('Flutter')],
           ),
         ));
+  }
+}
+
+class ProgressBarExample extends StatelessWidget {
+  const ProgressBarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+          child: Column(
+        children: [
+          const SizedBox(height: 100),
+          CircularProgressIndicator(
+            backgroundColor: Colors.blue[600],
+            valueColor: AlwaysStoppedAnimation(Colors.blue[100]),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: LinearProgressIndicator(
+              backgroundColor: Colors.pink[600],
+              valueColor: AlwaysStoppedAnimation(Colors.pink[100]),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 30),
+            child: CircleAvatar(
+              radius: 100.0,
+              backgroundImage: NetworkImage(
+                  'https://i.namu.wiki/i/2437azFTlVtr6KG0V3RbgeyZA43geIaK4BRyQ5OFey_K1Dfy3xpp55efrc6TlyhkNV_WJIUdJ6e-YBX4NB8Unw.webp'),
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
 
